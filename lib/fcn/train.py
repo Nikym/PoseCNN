62e7381975d76316353a54baccd975923a0df3e8
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-03-17 19:20:11 +0000

    Uncommented old summary

--- lib/fcn/train.py
+++ lib/fcn/train.py
@@ -239,8 +239,8 @@ class SolverWrapper(object):
 
             timer.tic()
             loss_value, loss_cls_value, loss_vertex_value, loss_pose_value, lr, _ = sess.run([loss, loss_cls, loss_vertex, loss_pose, learning_rate, train_op])
-            # train_writer.add_summary(summary, iter)
-            train_writer.add_summary(loss)
+            train_writer.add_summary(summary, iter)
+            #train_writer.add_summary(loss)
             timer.toc()
             print 'iter: %d / %d, loss: %.4f, loss_cls: %.4f, loss_vertex: %.4f, loss_pose: %.4f, lr: %.8f,  time: %.2f' %\
                     (iter+1, max_iters, loss_value, loss_cls_value, loss_vertex_value, loss_pose_value, lr, timer.diff)
